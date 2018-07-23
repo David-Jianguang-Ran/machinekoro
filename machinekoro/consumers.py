cb4885c25964c22a258799d2d59aaa98ce8a281c
David Ham <ham.meticulously@gmail.com>
2018-07-23 10:07:11 -0600

    Work in progress (lobby phase) PlayerConsumer

--- machinekoro/consumers.py
+++ machinekoro/consumers.py
@@ -1,5 +1,5 @@
-from asgiref.sync import async_to_sync,sync_to_async
-from channels.generic.websocket import SyncConsumer,AsyncConsumer,AsyncJsonWebsocketConsumer
+from asgiref.sync import async_to_sync, sync_to_async
+from channels.generic.websocket import SyncConsumer, AsyncJsonWebsocketConsumer
 
 import asyncio
 import copy
@@ -24,26 +24,94 @@ class PlayerWSConsumer(AsyncJsonWebsocketConsumer):
 
     methods callable by channels:
 
-    - prime.register.update
+    - send_json
+
+    - prime_register_update
+
+    - new_query_set
 
     methods callable by client:
 
-
+    - prime_player_command
 
     methods available only to prime player:
 
 
     """
     async def connect(self):
+        """
+        This method is called on connection with ws
+        It takes the token from url, gets a register obj and copies it to memory
+        it also updates the token record with new channel name
+        :return: None
+        """
+        # call game controller function to retrieve content
+        token = self.scope['url_route']['kwargs']['token']
+        register_content = sync_to_async(controllers.MatchController.initialize_by_token(token,self.channel_name))
+
+        self.register = register_content
+        match_id = self.register['match_id']
+
+        await self.accept()
+        await self.channel_layer.group_add(match_id)
+        pass
+
+    async def prime_register_update(self,event):
+        """
+        this method receives updates of prime_register obj send from MatchController.add_player_to_match
+        if the player is prime, then the prime_register gets saved
+        then prime_register gets forwarded to client with key register_update
+        :param event: event obj from channels
+        :return:
+        """
+        content = event['content']
+
+        # prime player consumer keeps the whole register in memory for routing
+        if self.register['is_prime']:
+            self.register_prime = content
+
+        # forward update to client withe the appropriate key
+        client_massage = {
+            "key": "register_update",
+            "content": content
+        }
+        await self.send_json(client_massage)
+
+    async def prime_player_command(self, message):
+        """
+        This methods handles all client prime player command
+        functions are called based on key of the client message:
+            "start_game" : send process request to GameProcessor
+            "add_bot" : add bot player register using MatchController
+        :param message: msg with key "prime.player.command"
+        :return:
+        """
+        key = message['key']
+        if key == "start_game" and self.register['is_prime']:
+            # send channel_layer initial turn  process request msg to GameProcessor
+            pass
+        elif key == "add_bot" and self.register['is_prime']:
+            match_id = self.register['match_id']
+            none_value = sync_to_async(controllers.MatchController.add_player_to_match(match_id,prime=False,bot=True))
+        else:
+            print(str(self.__doc__)+"prime command ignored key="+key)
         pass
 
     async def disconnect(self, code):
-        pass
-
-    async def send_json(self, content, close=False):
-        pass
+        if self.register['is_prime']:
+            pass
+        else:
+            pass
 
     async def receive_json(self, content, **kwargs):
+        """
+        This method takes message from ws connection (client) and routes based on type
+        :param content:
+        :param kwargs:
+        :return:
+        """
+        if content['type'] == 'prime.player.command':
+            await self.prime_player_command(content)
         pass
 
 
@@ -61,8 +129,8 @@ class GameProcessorConsumer(SyncConsumer):
     - process_response
 
     """
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.game_controller = None
 
     async def initial_turn(self,event):
--- machinekoro/views.py
+++ machinekoro/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render,redirect
 
 from . import controllers
 
+
 # Create your views here.
-
-
-# place holder views
-
 def cover_view(request):
     return render(request, 'static.html')
 
 
 def new_game_poke(request):
+    """
+    This function makes new MatchSession db object AND
+    new TokenRegister db object when poked,
+    then it redirects client to main_view with kwarg token = uuid string
+    :param request:
+    :return:
+    """
     match_controller = controllers.MatchController()
     match_id = match_controller.initialize_new_match()
     token = match_controller.add_player_to_match(match_id, prime=True)
@@ -19,12 +23,24 @@ def new_game_poke(request):
 
 
 def join_game_poke(request, match_id):
+    """
+    This function makes new TokenRegister db object when poked,
+    then redirects client to main_view with kwarg token = uuid string
+    :param request:
+    :return:
+    """
     match_controller = controllers.MatchController()
     token = match_controller.add_player_to_match(match_id, prime=False)
     return redirect(main_view,token=token)
 
 
 def main_view(request, token):
+    """
+    This function renders 'the one template to rule them all' with the context token passed in
+    :param request:
+    :param token:
+    :return:
+    """
     context={
         "token":token
     }
